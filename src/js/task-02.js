645ac654a327a78f61b4a200c0feb0fa4eb8ced6
Ront31 <kolya.maslov.09@gmail.com>
2023-02-14 19:17:59 +0200

    task 2 done

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulEl = document.querySelector("#ingredients");
+
+for (let i = 0; i < ingredients.length; i++) {
+  const liElCreate = document.createElement("li");
+  liElCreate.textContent = ingredients[i];
+  liElCreate.classList.add("item");
+  ulEl.append(liElCreate);
+}
