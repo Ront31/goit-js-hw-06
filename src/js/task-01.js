7eb889af1a77c839506a59e101a8f5b651892a46
Ront31 <kolya.maslov.09@gmail.com>
2023-02-14 18:05:39 +0200

    done 1 from task 1

---
+++ src/js/task-01.js
@@ -0,0 +1,5 @@
+const liElLength = document.querySelectorAll(".item").length;
+console.log(`Number of categories: ${liElLength}`);
+
+// const liInnerElLength = document.querySelectorAll("li").length;
+// console.log(liInnerElLength);
