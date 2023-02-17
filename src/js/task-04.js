184b6316afb0dff0b4b898c4cbb70895a82f1624
Ront31 <kolya.maslov.09@gmail.com>
2023-02-17 17:13:14 +0200

    4 done

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,2 +1,15 @@
-let counterValue = document.querySelector("#value");
-console.log(counterValue);
+let counterValue = 0;
+
+const spanCounter = document.querySelector("#value");
+const buttonMinus = document.querySelector('button[data-action="decrement"]');
+const buttonPlus = document.querySelector('button[data-action="increment"]');
+
+buttonMinus.addEventListener("click", () => {
+  counterValue -= 1;
+  spanCounter.textContent = counterValue;
+});
+
+buttonPlus.addEventListener("click", () => {
+  counterValue += 1;
+  spanCounter.textContent = counterValue;
+});
